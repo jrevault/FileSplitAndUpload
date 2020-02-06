4ecc6ff1607a5bd3a5da122cdc10f1fe740d2dc9
jrevault <jrevault@gmail.com>
2020-02-06 07:46:34 +0100

    Just tests, let's start

--- src/main/java/file/upload/endpoints/UploadEndpoint.java
+++ src/main/java/file/upload/endpoints/UploadEndpoint.java
@@ -9,6 +9,7 @@ import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.StreamingFileUpload;
 
 import javax.inject.Inject;
+import java.io.IOException;
 
 @Controller( "/upload" )
 public class UploadEndpoint {
@@ -18,9 +19,41 @@ public class UploadEndpoint {
 
   @Consumes( MediaType.MULTIPART_FORM_DATA )
   @Post( "/" )
-  public HttpResponse upload( StreamingFileUpload file ) {
+  public HttpResponse upload( StreamingFileUpload file ) throws IOException {
     file_services.go( file );
     return HttpResponse.ok( );
   }
+
+//  @Consumes( MediaType.MULTIPART_FORM_DATA )
+//  @Post( "/" )
+//  public HttpResponse upload( Publisher<StreamingFileUpload> file ) throws IOException {
+//    file.subscribe(new Subscriber<StreamingFileUpload>() {
+//      protected Subscription s;
+//
+//      @Override
+//      public void onSubscribe(Subscription s) {
+//        System.out.println("onSubscribe: " + s);
+//        this.s = s;
+//        s.request(1);
+//      }
+//
+//      @Override
+//      public void onNext(StreamingFileUpload streamingFileUpload) {
+//        System.out.println("onNext: {}" + streamingFileUpload);
+//        s.request(1);
+//      }
+//
+//      @Override
+//      public void onError(Throwable t) {
+//        System.out.println("onError: " + t);
+//      }
+//
+//      @Override
+//      public void onComplete() {
+//        System.out.println("onComplete");
+//      }
+//    });
+//    return HttpResponse.ok( );
+//  }
 
 }
--- src/main/java/file/upload/repository/FileServices.java
+++ src/main/java/file/upload/repository/FileServices.java
@@ -2,18 +2,32 @@ package file.upload.repository;
 
 import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.http.multipart.StreamingFileUpload;
+import io.reactivex.Single;
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
+import java.io.File;
+import java.io.IOException;
 
 @Singleton
 public class FileServices {
 
   Logger logger = LoggerFactory.getLogger( FileServices.class );
 
-  public void go( StreamingFileUpload file ) {
+  public Single<Boolean> go( StreamingFileUpload file ) throws IOException {
 
+    File tempFile = File.createTempFile( file.getFilename( ) , "temp" );
+    Publisher<Boolean> uploadPublisher = file.transferTo( tempFile );
+    return Single.fromPublisher( uploadPublisher )
+        .map( success -> {
+          if ( success ) {
+            return true;
+          } else {
+            return false;
+          }
+        } );
     // Init kafka connection
 
 //    Flowable.fromPublisher( file )
