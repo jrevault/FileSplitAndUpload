b947dd34e52488f3318e8e17f4266f0a8ac20352
jrevault <jrevault@gmail.com>
2020-02-07 07:50:00 +0100

    Upload basics

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Micronaut_FileUpload
+
+curl -H "Content-Type:multipart/form-data" -X POST -F "file=@01.pdf" http://localhost:8080/upload/stream
--- src/main/java/file/upload/endpoints/UploadEndpoint.java
+++ src/main/java/file/upload/endpoints/UploadEndpoint.java
@@ -1,59 +1,60 @@
 package file.upload.endpoints;
 
+import file.upload.MyProperties;
 import file.upload.repository.FileServices;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.StreamingFileUpload;
+import io.reactivex.Single;
+import org.reactivestreams.Publisher;
 
 import javax.inject.Inject;
-import java.io.IOException;
+import java.io.File;
 
 @Controller( "/upload" )
 public class UploadEndpoint {
 
-  @Inject
+  private MyProperties properties;
   private FileServices file_services;
 
-  @Consumes( MediaType.MULTIPART_FORM_DATA )
-  @Post( "/" )
-  public HttpResponse upload( StreamingFileUpload file ) throws IOException {
-    file_services.go( file );
-    return HttpResponse.ok( );
+  @Inject
+  public UploadEndpoint( MyProperties properties , FileServices file_services ) {
+    this.properties = properties;
+    this.file_services = file_services;
   }
 
-//  @Consumes( MediaType.MULTIPART_FORM_DATA )
-//  @Post( "/" )
-//  public HttpResponse upload( Publisher<StreamingFileUpload> file ) throws IOException {
-//    file.subscribe(new Subscriber<StreamingFileUpload>() {
-//      protected Subscription s;
-//
-//      @Override
-//      public void onSubscribe(Subscription s) {
-//        System.out.println("onSubscribe: " + s);
-//        this.s = s;
-//        s.request(1);
-//      }
-//
-//      @Override
-//      public void onNext(StreamingFileUpload streamingFileUpload) {
-//        System.out.println("onNext: {}" + streamingFileUpload);
-//        s.request(1);
-//      }
-//
-//      @Override
-//      public void onError(Throwable t) {
-//        System.out.println("onError: " + t);
-//      }
-//
-//      @Override
-//      public void onComplete() {
-//        System.out.println("onComplete");
-//      }
-//    });
-//    return HttpResponse.ok( );
-//  }
+  //Try HttpResponse upload(StreamingFileUpload file, Map<String, Object> metadata) { without @Body.
+
+  @Post( "/stream" )
+  @Consumes( MediaType.MULTIPART_FORM_DATA )
+  public Single<HttpResponse<String>> stream( StreamingFileUpload file ) {
+
+    long start = System.currentTimeMillis( );
+
+    File final_file = properties.getDestination( file.getFilename( ) ).toFile( );
+
+    Publisher<Boolean> uploadPublisher = file.transferTo( final_file );
+    return Single.fromPublisher( uploadPublisher )
+        .map( success -> {
+          if ( !success ) {
+            return HttpResponse.<String>status( HttpStatus.CONFLICT ).body( final_file.getAbsolutePath( ) );
+          }
+          else {
+            float duration = ( System.currentTimeMillis( ) - start ) / 1000;
+            float size = final_file.length( ) / 1000;
+            float throughput = size / duration;
+
+            System.out.println( "Full size  : " + size + " mb" );
+            System.out.println( "Duration   : " + duration + " s" );
+            System.out.println( "Throughput : " + throughput + " mb/s" );
+            return HttpResponse.ok( final_file.getAbsolutePath( ) );
+          }
+        } );
+  }
+
 
 }
